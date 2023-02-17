cda9085211bcb194ccb520c9dc800e9a6a943c08
Valen <valen@valen.wang>
2023-02-17 18:19:54 +0800

    ✨ feat(custom): impl basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,72 @@
+import { Field, Form, Formik } from "formik";
 import "./App.css";
 import SyntheticRoute from "./components/SyntheticRoute";
+
+interface FormValues {
+  target: string;
+  max_rxn_steps?: number;
+  n_path?: number;
+  max_material_cost?: number;
+  exclude_substructures?: string[];
+  exclude_materials?: string[];
+  exclude_rxns?: string[];
+}
 
 function App() {
   return (
     <div className="App">
+      <Formik<FormValues>
+        initialValues={{
+          target: "CCOC(=O)C(F)C(NCC(C)C)",
+          max_rxn_steps: 3,
+          n_path: 5,
+        }}
+        onSubmit={() => {}}
+      >
+        <Form>
+          <label htmlFor="target">目标分子</label>
+          <Field id="target" name="target" />
+          <br />
+
+          <label htmlFor="max_rxn_steps">最大实验数</label>
+          <Field id="max_rxn_steps" name="max_rxn_steps" type="number" />
+          <br />
+
+          <label htmlFor="n_path">路径数</label>
+          <Field id="n_path" name="n_path" type="number" />
+          <br />
+
+          <label htmlFor="max_material_cost">最大物料花费</label>
+          <Field
+            id="max_material_cost"
+            name="max_material_cost"
+            type="number"
+          />
+          <br />
+
+          <label htmlFor="exclude_substructures">排除分子</label>
+          <Field
+            id="exclude_substructures"
+            name="exclude_substructures"
+            type="number"
+          />
+          <br />
+
+          <label htmlFor="exclude_materials">排除物料</label>
+          <Field
+            id="exclude_materials"
+            name="exclude_materials"
+            type="number"
+          />
+          <br />
+
+          <label htmlFor="exclude_rxns">排除反应</label>
+          <Field id="exclude_rxns" name="exclude_rxns" type="number" />
+          <br />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
       <SyntheticRoute routes={routes} />
     </div>
   );
